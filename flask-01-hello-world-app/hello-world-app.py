d4c1ac64d17009b533bff4fed513fef4b4369646
Vincenzo <vncz.17@gmail.com>
2021-03-14 01:04:22 +0300

    jinja templates

---
+++ flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return "Hello World!"
+
+@app.route('/irfan')
+def subtitle():
+    return "Bize Her Yer Trabzon"
+
+@app.route('/irfan/subirfan')
+def subsub():
+    return "This is the subpage of third page"
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
+
+if __name__ == '__main__':
+    app.run(debug = True)
